4ffd0e6bff41854e80ec72f8ad66d57f178801db
Kevin Bradwick <kevinbradwick@gmail.com>
2023-02-24 21:12:23 +0000

    added union find

---
+++ algorithms/union_find.py
@@ -0,0 +1,63 @@
+from typing import List
+
+
+class UnionFind:
+    def __init__(self, size: int) -> None:
+        """
+        Initialize a UnionFind data structure with size elements.
+        """
+        # the index of the element is the element itself and the value is an id that is
+        # unique for each connected component. -1 means that the element is not connected
+        # to any other element.
+        self.index: List[int] = [-1] * size
+
+    def union(self, a: int, b: int) -> None:
+        """
+        Union two elements.
+        This will change the id of (a) and all connected nodes to the id of (b).
+        """
+
+        a_id = self.index[a]
+        b_id = self.index[b]
+
+        if b_id == -1:
+            self.index[b] = b_id = b
+
+        self.index[a] = b_id
+
+        for i, v in enumerate(self.index):
+            if v != -1 and v == a_id:
+                self.index[i] = b_id
+
+    def connected(self, p: int, q: int) -> bool:
+        """
+        Check if two elements are connected.
+        """
+        if self.index[p] == -1 or self.index[q] == -1:
+            return False
+
+        try:
+            return self.index[p] == self.index[q]
+        except IndexError:
+            return False
+
+
+def test_union_find(a: int, b: int) -> bool:
+    """
+    Test function for the union find data structure.
+    >>> test_union_find(1, 3)
+    True
+    >>> test_union_find(1, 5)
+    False
+    >>> test_union_find(5, 9)
+    True
+    >>> test_union_find(1, 7)
+    True
+    """
+    uf = UnionFind(10)
+    uf.union(1, 2)
+    uf.union(2, 3)
+    uf.union(5, 9)
+    uf.union(1, 7)
+
+    return uf.connected(a, b)
